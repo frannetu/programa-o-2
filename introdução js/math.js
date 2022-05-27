f4cb697b7f04abed90b7a65c7258f8c991cd678b
frannetu <franciscodias1718@gmail.com>
2022-05-27 16:43:02 -0300

    math.js

---
+++ introdução js/math.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+console.log("o comando Math.PI retorna o numero pi ");
+console.log("O valor de pi é: "+Math.PI );
+
+console.log("o comando Math.sqrt(x) retorna a raiz quadrada de x ");
+console.log("a raiz i é quadrada de 64 é: "+Math.sqrt(64) );
+
+console.log("o comando Math.pow(x,y) retorna o valor x vezes o y ");
+console.log("quanto e 16 * 38: "+Math.pow(16,38) );
+
+
